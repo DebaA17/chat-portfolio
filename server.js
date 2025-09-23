1ba5f579b2205493c26e312d3560b70388fe0583
DEBASIS BISWAS <185465407+DebaA17@users.noreply.github.com>
2025-09-23 09:24:59 +0530

    Fix commented-out path import in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-#const path = require('path');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
